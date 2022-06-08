95f6c93b148cc9d6443f585f08b55e0ad2de6ba5
SHAHZAIB-PC <shahzaiba9872@gmail.com>
2022-06-08 10:42:36 +0500

    June 8,2022

--- app/src/main/java/com/smartscheduler_admin/fragments/schedule/AddNewScheduleFragment.java
+++ app/src/main/java/com/smartscheduler_admin/fragments/schedule/AddNewScheduleFragment.java
@@ -23,6 +23,8 @@ import com.google.firebase.database.ValueEventListener;
 import com.smartscheduler_admin.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
 
 public class AddNewScheduleFragment extends Fragment {
     EditText etDay;
@@ -38,7 +40,7 @@ public class AddNewScheduleFragment extends Fragment {
 
     Button addNewSchedule;
 
-    ArrayAdapter<String> adapterCourse,adapterFaculty,adapterRoom,adapterSemester,adapterDepartment;
+    ArrayAdapter<String> adapterCourse, adapterFaculty, adapterRoom, adapterSemester, adapterDepartment;
 
     DatabaseReference myRef;
     FirebaseAuth mAuth;
@@ -49,8 +51,18 @@ public class AddNewScheduleFragment extends Fragment {
     ArrayList<String> SemesterList = new ArrayList<>();
     ArrayList<String> DepartmentList = new ArrayList<>();
 
-    int count=0;
-    boolean isCountingDone=false;
+    int count = 0;
+    boolean isCountingDone = false;
+
+    String day;
+    String creditHour;
+    String startingTime;
+    String endingTime;
+    String course;
+    String faculty;
+    String room;
+    String department;
+    String semester;
 
     public AddNewScheduleFragment() {
         // Required empty public constructor
@@ -132,83 +144,58 @@ public class AddNewScheduleFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
 
         addNewSchedule.setOnClickListener(v -> {
-            if (!isCountingDone){
+            if (!isCountingDone) {
                 return;
             }
 
-            String day = etDay.getText().toString().trim();
-            String creditHour = etCreditHour.getText().toString().trim();
-            String startingTime = etStartingTime.getText().toString().trim();
-            String endingTime = etEndingTime .getText().toString().trim();
+            day = etDay.getText().toString().toLowerCase(Locale.ROOT).trim();
+            creditHour = etCreditHour.getText().toString().toLowerCase(Locale.ROOT).trim();
+            startingTime = etStartingTime.getText().toString().toLowerCase(Locale.ROOT).trim();
+            endingTime = etEndingTime.getText().toString().toLowerCase(Locale.ROOT).trim();
+            course = CourseSpinner.getSelectedItem().toString().toLowerCase(Locale.ROOT);
+            faculty = FacultySpinner.getSelectedItem().toString().toLowerCase(Locale.ROOT);
+            room = RoomSpinner.getSelectedItem().toString().toLowerCase(Locale.ROOT);
+            department = DepartmentSpinner.getSelectedItem().toString().toLowerCase(Locale.ROOT);
+            semester = SemesterSpinner.getSelectedItem().toString().toLowerCase(Locale.ROOT);
 
-            String course = CourseSpinner.getSelectedItem().toString();
-            String faculty = FacultySpinner.getSelectedItem().toString();
-            String room = RoomSpinner.getSelectedItem().toString();
-            String department = DepartmentSpinner.getSelectedItem().toString();
-            String semester = SemesterSpinner.getSelectedItem().toString();
-
-            if (day.equals("")) {
+            if (day.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Add Day", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (creditHour.equals("")) {
+            if (creditHour.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Add Credit Hour", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (startingTime.equals("")) {
+            if (startingTime.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Add Starting Time", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (endingTime.equals("")) {
+            if (endingTime.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Add Ending Time", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (course.equals("")) {
+            if (course.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Select Course", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (faculty.equals("")) {
+            if (faculty.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Select Faculty", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (room.equals("")) {
+            if (room.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Select Room", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (semester.equals("")) {
+            if (semester.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Select Semester", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (department.equals("")) {
+            if (department.equalsIgnoreCase("")) {
                 Toast.makeText(getContext(), "Select Department", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            DatabaseReference newRef = myRef.child("Schedule").child((++count)+"");
-
-            newRef.child("ID").setValue(count);
-            newRef.child("DAY").setValue(day);
-            newRef.child("S_TIME").setValue(startingTime);
-            newRef.child("E_TIME").setValue(endingTime);
-            newRef.child("COURSE").setValue(course);
-            newRef.child("FACULTY").setValue(faculty);
-            newRef.child("ROOM").setValue(room);
-            newRef.child("SEMESTER").setValue(semester);
-            newRef.child("CREDIT_HOUR").setValue(creditHour);
-            newRef.child("DEPARTMENT").setValue(department);
-
-            Toast.makeText(getContext(), "Schedule Added", Toast.LENGTH_SHORT).show();
-
-            etDay.setText("");
-            etCreditHour .setText("");
-            etStartingTime.setText("");
-            etEndingTime .setText("");
-
-            CourseSpinner.setSelection(0);
-            FacultySpinner.setSelection(0);
-            RoomSpinner .setSelection(0);
-            SemesterSpinner .setSelection(0);
-            DepartmentSpinner.setSelection(0);
+            CheckOldData(day, creditHour, startingTime, endingTime, course, faculty, room, department, semester);
         });
 
         getScheduleNumber();
@@ -216,14 +203,83 @@ public class AddNewScheduleFragment extends Fragment {
         return view;
     }
 
-    private void getScheduleNumber(){
+    private void SaveDB() {
+        DatabaseReference newRef = myRef.child("Schedule").child((++count) + "");
+
+        newRef.child("ID").setValue(count);
+        newRef.child("DAY").setValue(day);
+        newRef.child("S_TIME").setValue(startingTime);
+        newRef.child("E_TIME").setValue(endingTime);
+        newRef.child("COURSE").setValue(course);
+        newRef.child("FACULTY").setValue(faculty);
+        newRef.child("ROOM").setValue(room);
+        newRef.child("SEMESTER").setValue(semester);
+        newRef.child("CREDIT_HOUR").setValue(creditHour);
+        newRef.child("DEPARTMENT").setValue(department);
+
+        Toast.makeText(getContext(), "Schedule Added", Toast.LENGTH_SHORT).show();
+
+        etDay.setText("");
+        etCreditHour.setText("");
+        etStartingTime.setText("");
+        etEndingTime.setText("");
+
+        CourseSpinner.setSelection(0);
+        FacultySpinner.setSelection(0);
+        RoomSpinner.setSelection(0);
+        SemesterSpinner.setSelection(0);
+        DepartmentSpinner.setSelection(0);
+    }
+
+    private void CheckOldData(String day, String creditHour, String startingTime,
+                              String endingTime, String course, String faculty, String room,
+                              String department, String semester) {
         DatabaseReference newRef = myRef.getRef();
         newRef.child("Schedule").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                isCountingDone=true;
+                boolean exist = false;
+                for (DataSnapshot s: snapshot.getChildren()) {
+                    String DAY = s.child("DAY").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String S_TIME = s.child("S_TIME").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String E_TIME = s.child("E_TIME").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String COURSE = s.child("COURSE").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String FACULTY = s.child("FACULTY").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String ROOM = s.child("ROOM").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String SEMESTER = s.child("SEMESTER").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String CREDIT_HOUR = s.child("CREDIT_HOUR").getValue(String.class).toLowerCase(Locale.ROOT);
+                    String DEPARTMENT = s.child("DEPARTMENT").getValue(String.class).toLowerCase(Locale.ROOT);
+
+                    if (Objects.equals(DAY, day) && Objects.equals(S_TIME, startingTime)
+                            && Objects.equals(E_TIME, endingTime) && Objects.equals(COURSE, course)
+                            && Objects.equals(FACULTY, faculty) && Objects.equals(ROOM, room)
+                            && Objects.equals(SEMESTER, semester) && Objects.equals(CREDIT_HOUR, creditHour)
+                            && Objects.equals(DEPARTMENT, department))
+                    {
+                        Toast.makeText(getContext(), "Already added Time Table to this Slot !", Toast.LENGTH_LONG).show();
+                        exist = true ;
+                        break;
+                    }
+                }
+                if (!exist)
+                    SaveDB();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getScheduleNumber() {
+        DatabaseReference newRef = myRef.getRef();
+        newRef.child("Schedule").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                isCountingDone = true;
                 if (snapshot.exists())
-                    count=(int) snapshot.getChildrenCount();
+                    count = (int) snapshot.getChildrenCount();
             }
 
             @Override
